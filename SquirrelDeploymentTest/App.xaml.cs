64f5efeabb5d282aef9bf3bd6e4b05f4c7e1e95c
Victor Zlatarev <vicizlat@gmail.com>
2022-06-23 12:46:21 +0300

    Initial commit

---
+++ SquirrelDeploymentTest/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace SquirrelDeploymentTest
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ SquirrelDeploymentTest/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using Squirrel;
+
+namespace SquirrelDeploymentTest
+{
+    public partial class MainWindow : Window
+    {
+        UpdateManager manager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/Vicizlat/SquirrelDeploymentTest");
+
+            CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
+        }
+
+        private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateInfo updateInfo = await manager.CheckForUpdate();
+
+            if (updateInfo.ReleasesToApply.Count > 0)
+            {
+                UpdateButton.IsEnabled = true;
+            }
+            else
+            {
+                UpdateButton.IsEnabled = false;
+            }
+        }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            await manager.UpdateApp();
+
+            MessageBox.Show("Updated succesfuly!");
+        }
+    }
+}
