4aa7351fe51960866404040de01a5428d340a1b1
Victor Zlatarev <vicizlat@gmail.com>
2022-06-23 13:38:52 +0300

    1.0.2

--- SquirrelDeploymentTest/MainWindow.xaml.cs
+++ SquirrelDeploymentTest/MainWindow.xaml.cs
@@ -15,29 +15,21 @@ namespace SquirrelDeploymentTest
         private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/Vicizlat/SquirrelDeploymentTest");
-            Title = "Squirrel Deployment Test v." + manager.CurrentlyInstalledVersion().ToString();
 
+            Title = "Squirrel Deployment Test v." + manager.CurrentlyInstalledVersion().ToString();
             CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
+            InstallDir.Text = manager.RootAppDirectory;
         }
 
         private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
         {
             UpdateInfo updateInfo = await manager.CheckForUpdate();
-
-            if (updateInfo.ReleasesToApply.Count > 0)
-            {
-                UpdateButton.IsEnabled = true;
-            }
-            else
-            {
-                UpdateButton.IsEnabled = false;
-            }
+            UpdateButton.IsEnabled = updateInfo.ReleasesToApply.Count > 0;
         }
 
         private async void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
             await manager.UpdateApp();
-
             MessageBox.Show("Updated succesfuly!");
         }
     }
